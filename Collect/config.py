2998d24b09d46cbc40aea9f4e95885dc53ef3a50
zeuslabs <jinsoomail@gmail.com>
2019-06-04 20:28:32 +0900

    Update config.py

--- Collect/config.py
+++ Collect/config.py
@@ -9,11 +9,3 @@ class Source:
     BITHUMB_URI = 'https://api.bithumb.com/public/ticker/'
     COINONE_URI = 'https://api.coinone.co.kr/ticker/'
     KORBIT_URI = 'https://api.korbit.co.kr/v1/ticker/detailed'
-
-    # failed_alert = SlackAPIPostOperator(
-    #     task_id='slack_failed',
-    #     channel="#airflow_slack_swkim",
-    #     token="xoxp-548248898820-551665805383-608585317927-587de295c16747b7a15162532da04f79",
-    #     text = ':red_circle: Task Failed',
-    #     username = 'airflow',)
-    # return failed_alert.execute(context=context)
