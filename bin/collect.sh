6336a0e888172a60ce8b39b38254745ef2fb0a3e
zeus <jinsoomail@gmail.com>
2019-06-09 04:38:28 +0900

    update

--- bin/collect.sh
+++ bin/collect.sh
@@ -5,6 +5,8 @@ if [ -f ~/.bashrc ]; then
 fi
 
 ((
-  cd ./Collect/ && \
-  python main.py
+  ./Collect/python main.py;
+  git add .;
+  git commit -m "update";
+  git push
 ) > ./log/collect_$(date '+%Y%m%d%H%M%S').log 2>&1)
