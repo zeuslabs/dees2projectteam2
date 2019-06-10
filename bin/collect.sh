755b509493278e1699ba8c0b66c800330a299976
zeus <jinsoomail@gmail.com>
2019-06-10 10:42:52 +0900

    update

--- bin/collect.sh
+++ bin/collect.sh
@@ -7,8 +7,8 @@ fi
 ((
   python ./Collect/main.py;
   date +'%Y-%m-%d %X';
-  git add .;
-  git commit -m "update";
-  git push;
+#  git add .;
+#  git commit -m "update";
+#  git push;
   date +'%Y-%m-%d %X';
 ) > ./log/collect_$(date '+%Y%m%d%H%M%S').log 2>&1)
