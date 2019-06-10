1846dd4dab1382e7264af361607512a066e10b2e
zeus <jinsoomail@gmail.com>
2019-06-10 10:49:57 +0900

    auto git push

--- bin/gitpush.sh
+++ bin/gitpush.sh
@@ -5,12 +5,8 @@ if [ -f ~/.bashrc ]; then
 fi
 
 ((
-  python ./Collect/main.py;
   date +'%Y-%m-%d %X';
-
   git add .;
   git commit -m "auto git push";
   git push origin master -f;
-  date +'%Y-%m-%d %X';
-
-) >> ./log/collect_$(date '+%Y%m%d%H').log 2>&1)
+) >> ./log/collect_$(date '+%Y%m%d').log 2>&1)
