c6aa80cd5a3265c815a1a3c6e1f43a86edc10731
zeus <jinsoomail@gmail.com>
2019-06-10 10:56:46 +0900

    auto git push

--- bin/collect.sh
+++ bin/collect.sh
@@ -6,13 +6,19 @@ fi
 
 ((
 
-  echo 'start run'
-  python ./Collect/main.py;
+  echo '         '
+  echo '=================================================='
+  echo '[[start task]]'
   date +'%Y-%m-%d %X';
+  python ./Collect/main.py;
 
+  echo '[[git run]]'
+  date +'%Y-%m-%d %X';
   git add .;
   git commit -m "auto git push";
   git push origin master -f;
+
+  echo '[[end task]]'
   date +'%Y-%m-%d %X';
 
 ) >> ./log/collect_$(date '+%Y%m%d%H').log 2>&1)
